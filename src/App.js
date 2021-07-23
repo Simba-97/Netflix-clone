aa5a64e7ce650839d1a879b3f5b990abe9a8e675
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-07-23 22:56:13 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/Row';
+import requests from './request';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Webflix</h1>
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} isLargeRow={true}/>
+      <Row title="Trending now" fetchURL={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>    
     </div>
   );
 }
---
+++ src/request.js
@@ -0,0 +1,14 @@
+
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=55`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`
+}
+
+export default requests;
